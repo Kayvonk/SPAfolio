53f958182b8addadadbb960df7d85a6300acab19
Kayvon Kazemini <kayvonk@gmail.com>
2021-06-02 16:57:02 -0600

    Updated Resume

--- src/components/CardBtn/index.js
+++ src/components/CardBtn/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./style.css";
 
 function CardBtn() {
-  return <a className="btn btn-info btn-lg resumeBtn" href="./assets/KayvonKazemini-4.27.21.pdf"
-    download="assets/Resume- KayvonKazemini-2.14.21.pdf" role="button">Download Resume</a>;
+  return <a className="btn btn-info btn-lg resumeBtn" href="./assets/Resume6.2.21.pdf"
+    download="./assets/Resume6.2.21.pdf" role="button">Download Resume</a>;
 }
 
 export default CardBtn;
